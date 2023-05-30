b8833d633a80e18acbd6535618af5d035448dbfc
Parker <parkied@gmail.com>
2023-05-30 11:38:34 -0700

    service worker refine

--- serviceWorker.js
+++ serviceWorker.js
@@ -1,28 +1,71 @@
-const todos = "todos-site"
-const assets = [
+const CACHE = `cache-1234`
+
+const ASSETS = [
     "/",
     "/index.html",
     "/css/style.css",
     "/icons/test.png",
-    "/icons/test3.png",
     "/app.js",
     "/modules/Controller.js",
     "/modules/Model.js",
     "/modules/View.js"
 ]
 
-self.addEventListener("install", installEvent => {
-    installEvent.waitUntil(
-        caches.open(todos)
-        .then(cache => {
-            cache.addAll(assets)
-        })
-    )
-})
+// self.addEventListener("fetch", fetchEvent => {
+//     fetchEvent.respondWith(
+//         caches.match(fetchEvent.request)
+//         .then(res => res || fetch(fetchEvent.request))
+//     )
+// })
 
-self.addEventListener("fetch", fetchEvent => {
-    fetchEvent.respondWith(
-        caches.match(fetchEvent.request)
-        .then(res => res || fetch(fetchEvent.request))
-    )
-})
+self.addEventListener('install', installEvent => {
+    // Create a new cache and add all files to it
+    async function addFilesToCache() {
+        const cache = await caches.open(CACHE);
+        await cache.addAll(ASSETS);
+    }
+
+    installEvent.waitUntil(addFilesToCache());
+});
+
+self.addEventListener('activate', activateEvent => {
+    // Remove previous cached data from disk
+    async function deleteOldCaches() {
+        for (const key of await caches.keys()) {
+            if (key !== CACHE) await caches.delete(key);
+        }
+    }
+
+    activateEvent.waitUntil(deleteOldCaches());
+});
+
+self.addEventListener('fetch', (event) => {
+    // ignore POST requests etc
+    if (event.request.method !== 'GET') return;
+
+    async function respond() {
+        const url = new URL(event.request.url);
+        const cache = await caches.open(CACHE);
+
+        // `build`/`files` can always be served from the cache
+        if (ASSETS.includes(url.pathname)) {
+            return cache.match(url.pathname);
+        }
+
+        // for everything else, try the network first, but
+        // fall back to the cache if we're offline
+        try {
+            const response = await fetch(event.request);
+
+            if (response.status === 200) {
+                cache.put(event.request, response.clone());
+            }
+
+            return response;
+        } catch {
+            return cache.match(event.request);
+        }
+    }
+
+    event.respondWith(respond());
+});
